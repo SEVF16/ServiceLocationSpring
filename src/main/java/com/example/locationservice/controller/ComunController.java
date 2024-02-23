96248aec187bd0692b769d29d5d08c49a01d8d20
Shiryu Valecillos Personal <shiryuevfr@gmail.com>
2024-02-23 12:00:47 -0300

    Se agrega cors para aceptar solicitud

--- src/main/java/com/example/locationservice/controller/ComunController.java
+++ src/main/java/com/example/locationservice/controller/ComunController.java
@@ -3,15 +3,13 @@ package com.example.locationservice.controller;
 import com.example.locationservice.domain.Comun;
 import com.example.locationservice.domain.service.ComunService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/comuna")
+@CrossOrigin(origins = "*")
 public class ComunController {
     @Autowired
     private ComunService comunService;
